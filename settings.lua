305c87a1a5b534849a336afce762367aac51f836
Antoine PLASKOWSKI <plaskowski.stanislas@gmail.com>
2025-05-19 16:51:36 +0200

    0.0.0

---
+++ settings.lua
@@ -0,0 +1,10 @@
+data:extend(
+  {
+    {
+      type = "bool-setting",
+      name = "electric-furnace-as-assembling-machine-all-furnaces",
+      setting_type = "startup",
+      default_value = false,
+    },
+  }
+)
