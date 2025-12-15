13acf8e49fbd309a493e066efe1e8369c38f0e27
Antoine PLASKOWSKI <plaskowski.stanislas@gmail.com>
2025-12-15 10:33:55 +0100

    0.1.1

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,6 +2,7 @@ if settings.startup["electric-furnace-as-assembling-machine-all-furnaces"].value
   for recipe_name, recipe in pairs(data.raw.recipe) do
     if string.find(recipe_name, "recycling") and recipe.hidden then
       recipe.hidden = false
+      recipe.hidden_in_factoriopedia = true
       recipe.hide_from_player_crafting = true
     end
   end
